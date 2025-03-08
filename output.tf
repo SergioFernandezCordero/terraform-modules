16a00c5849ce999e3404de4cee90335921482e40
Sergio Fernández <sergio@fernandezcordero.net>
2025-03-09 00:53:04 +0100

    Variables course

---
+++ output.tf
@@ -0,0 +1,18 @@
+output "nombrado" {
+    value = var.nombre
+}
+output "lista" {
+    value = var.set
+}
+
+output "producto" {
+    value = var.mapa.precio
+}
+
+output "objecto" {
+    value = var.objecto
+}
+
+output "mapobjecto" {
+    value = var.mapobjecto
+}
