de1c86e30696312d8046698ac36de0da95312f6d
Sergio Fernandez Cordero <sergio.fernandez.cordero@ing.com>
2026-04-14 14:56:37 +0200

    Sample resource group

--- modules/infra/main.tf
+++ modules/infra/main.tf
@@ -18,3 +18,7 @@ resource "azurerm_resource_group" "test" {
   location  = "westeurope"
   name      = "test-resource-group"
 }
+
+output "output_example" {
+  value = azurerm_resource_group.test.name
+}
